7a85a61472585a60a129f8115ee9a929772b4b98
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-06-09 19:02:38 -0400

    added starter code

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// will need to create page-template file below
+// const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+// const questions = [];
+
+
+// questions section part of first array - place into HTML according to Questions section
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
